9ed19d2e5b5a073e2fea5be09a4a2d39c322ae93
Julie Dursema <jmdursema@gmail.com>
2018-01-04 15:08:30 -0700

    display sworn members only on click

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,22 +2,13 @@ import React, { Component } from 'react';
 
 class Card extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
       isActive: this.props.active
     }
-    let swornMembers;
 
-    if (this.state.isActive){
-      swornMembers = this.props.swornMembers.map((member)=> {
-        const key = Object.keys(member)
-        return (
-          <p>{key}: {member[key]}</p>
-        
-     )
-    })
   }
-  }
+
 
   
 
@@ -30,25 +21,51 @@ class Card extends Component {
   }
   
 
-render(){
-  return (
-    <div 
-    className='card'
-    onClick={this.handleClick}>
-      <h2>
-        {this.props.name}
-      </h2>
-      <p>Founded: {this.props.founded}</p>
-      <p>Seats: {this.props.seats}</p>
-      <p>Titles: {this.props.titles}</p>
-      <p>Coat Of Arms: {this.props.coatOfArms}</p>
-      <p>Weapons: {this.props.ancestralWeapons}</p>
-      <p>Words: {this.props.words}</p>
-      <div>
+  render(){
+
+    if (!this.state.isActive){
+      return (
+        <div 
+          className='card'
+          onClick={this.handleClick}>
+          <h2>
+            {this.props.name}
+          </h2>
+          <p>Founded: {this.props.founded}</p>
+          <p>Seats: {this.props.seats}</p>
+          <p>Titles: {this.props.titles}</p>
+          <p>Coat Of Arms: {this.props.coatOfArms}</p>
+          <p>Weapons: {this.props.ancestralWeapons}</p>
+          <p>Words: {this.props.words}</p> 
+        </div>
+      );
+    } else {
+
+      return (
+        <div 
+        className='card'
+        onClick={this.handleClick}>
+        <h2>
+          {this.props.name}
+        </h2>
+        <p>Founded: {this.props.founded}</p>
+        <p>Seats: {this.props.seats}</p>
+        <p>Titles: {this.props.titles}</p>
+        <p>Coat Of Arms: {this.props.coatOfArms}</p>
+        <p>Weapons: {this.props.ancestralWeapons}</p>
+        <p>Words: {this.props.words}</p> 
+        <div>
+          {this.props.swornMembers.map((member)=> {
+           const keys= Object.keys(member)
+          return (<p>{keys}: {member[keys]}</p>)
+            }
+          )}
+        </div>
       </div>
-    </div>
-  );
-}
+      )
+    }
+    
+  }
   
 };
 
