c36266cc2966f6d9de2baaf3c49ff15fda5dbda9
Julie Dursema <jmdursema@gmail.com>
2018-01-04 10:39:51 -0700

    add loading screen

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Card from '../Card/Card.js';
+import './CardContainer.css'
 
 const CardContainer = props => {
-const mappedCards= props.Houses.map((house)=> {
+const mappedCards= props.houses.map((house)=> {
   return <Card
           name= {house.name}
           founded={house.founded}seats= {house.seats}
@@ -12,15 +13,24 @@ const mappedCards= props.Houses.map((house)=> {
           ancestralWeapons={house.ancestralWeapons}
           words={house.words}/>
 })
+
+if (props.houses[0]){
   return (
     <div>
       {mappedCards}
     </div>
   );
+} else {
+  return (
+    <div className='loading'>
+    </div>
+  )
+}
+  
 };
 
 const mapStateToProps = state => ({
-  Houses: state.houses
+  houses: state.houses
 })
 
 export default connect (mapStateToProps, null)(CardContainer);
