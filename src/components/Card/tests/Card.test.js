8850d3eb21def3183ed77a333a0bf95d878baff8
Julie Dursema <jmdursema@gmail.com>
2018-01-04 11:03:05 -0700

    write card test

---
+++ src/components/Card/tests/Card.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from '../Card';
+
+describe ('Card', () => {
+  it('should match the snapshot', () => {
+    const renderedCard = shallow(<Card
+      name= {'Stark'}
+      founded={'by Julie'}
+      seats= {'lots of um'}
+      titles= {'Kings of the North'}
+      coatOfArms={'Dire Wolf'}
+      ancestralWeapons={'Long Sword'}
+      words={'Winter is coming'}/>)
+
+    expect(renderedCard).toMatchSnapshot()
+  });
+});
