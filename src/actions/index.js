a7a35c5fcdcc3a341c1cb4a28d5712025af48299
Julie Dursema <jmdursema@gmail.com>
2018-01-04 09:47:42 -0700

    create action to store house data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
 export const fakeAction = () => ({type: 'FAKE'});
+
+export const fetchData = () => async (dispatch) => {
+  try {
+    const fetchGoTData = await fetch(`http://localhost:3001/api/v1/houses`)
+    const parsedData = await fetchGoTData.json()
+
+    dispatch (storeHouseData(parsedData))
+  } catch (error){
+    console.log(error)
+  }
+}
+
+export const storeHouseData = (arrayOfHouses) => ({ 
+  type: 'STORE_HOUSE_DATA',
+  arrayOfHouses
+})
